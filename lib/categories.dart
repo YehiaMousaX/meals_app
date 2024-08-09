ed4f0817a414a23928da54a7963cc553b2f64dbd
YehiaMousaX <118112478+YehiaMousaX@users.noreply.github.com>
2024-08-10 00:24:34 +0300

    156

--- lib/categories.dart
+++ lib/categories.dart
@@ -8,6 +8,40 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pick a Category'),
       ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text(
+            "data1",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "data2",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "data3",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "data4",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "data5",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "data6",
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
     );
   }
 }
