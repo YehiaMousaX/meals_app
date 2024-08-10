ae7ab66d104c17ab829f639e467b3987b994ac8a
YehiaMousaX <118112478+YehiaMousaX@users.noreply.github.com>
2024-08-10 05:31:08 +0300

    169

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -4,18 +4,58 @@ import 'package:meals_app/models/meals.dart';
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({super.key, required this.meal});
   final Meal meal;
+  
 
   @override
   Widget build(BuildContext context) {
+    int count = 0;
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 10),
+            for (final ingredient in meal.ingredients)
+              Text(ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      )),
+            const SizedBox(height: 10),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 10),
+            for (final entry in meal.steps.asMap().entries)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('${entry.key + 1}. ${entry.value}',
+                textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        )),
+              ),
+          ],
+        ),
       ),
     );
   }
